32572bad619c08faebfa9e4c5c3854901ace24ad
Angeliki. Bakogianni <angeliki.bakogianni@arup.com>
2016-04-13 17:19:53 +0100

    Small changes, files addeSmall changes, files addedd

--- wikihousearapp/www/js/app.js
+++ wikihousearapp/www/js/app.js
@@ -5,7 +5,7 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
+angular.module('wikihousear', ['ionic', 'wikihousear.controllers', 'wikihousear.services'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -77,7 +77,17 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
         controller: 'AccountCtrl'
       }
     }
-  });
+  })
+
+  //.state('tab.angeliki', {
+  //  url: '/angeliki',
+  //  views: {
+  //    'tab-angeliki': {
+  //      templateUrl: 'templates/tab-angeliki.html',
+        //controller: 'AccountCtrl'
+  //    }
+  //  }
+  //});
 
   // if none of the above states are matched, use this as the fallback
   $urlRouterProvider.otherwise('/tab/dash');
